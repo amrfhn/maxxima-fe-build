4351b1ef5cc645fe282ba678b831529393ddae05
ihlim97 <iehowe97@gmail.com>
2021-01-25 22:57:16 +0800

    remove :target scrolling

--- src/js/global.js
+++ src/js/global.js
@@ -1,19 +1,6 @@
 import ScrollOut from 'scroll-out';
 
 $(function () {
-    var target = $(':target').offset();
-    if (typeof target !== 'undefined') {
-        var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ? true : false;
-    
-        var scrollto = target.top - (isMobile ? 60 : 95);
-    
-        $('html, body').animate({ scrollTop: scrollto }, 0);
-    }
-})
-
-
-$(function () {
-
     $('.animated.text-slide-in').each(function () {
         $(this).html($(this).text().replace(/\S+/g, '<n>$&</n>'))
         $(this).find('n').attr('data-scroll', true)
