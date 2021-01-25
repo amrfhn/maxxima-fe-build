2c19f263c00638c75277bd3869c8f1c55724dfdb
ihlim97 <iehowe97@gmail.com>
2021-01-25 17:08:12 +0800

    added check for target that could be possibly undefined

--- src/js/global.js
+++ src/js/global.js
@@ -1,8 +1,10 @@
 $(function () {
     var target = $(':target').offset();
-    var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ? true : false;
-
-    var scrollto = target.top - (isMobile ? 60 : 95);
-
-    $('html, body').animate({ scrollTop: scrollto }, 0);
+    if (typeof target !== 'undefined') {
+        var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ? true : false;
+    
+        var scrollto = target.top - (isMobile ? 60 : 95);
+    
+        $('html, body').animate({ scrollTop: scrollto }, 0);
+    }
 })
