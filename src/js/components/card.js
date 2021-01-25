36922dbcb79355a722373b4bb6522d4c614088e3
Amir Farhan Jefri (DE-SWE/DIGITAL) <amirfarhan.jefri@PETRONAS.PETRONET.DIR>
2021-01-25 12:47:11 +0800

    Completed form and bullets swiper

--- src/js/components/card.js
+++ src/js/components/card.js
@@ -1,27 +1,27 @@
 $(function () {
     var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ? true : false;
-    var cardVar2 = $('.card-variant-1');
+    var cardVar1 = $('.card-variant-1');
 
-    if (!isMobile) {
-        cardVar2.each(function () {
+    // if (!isMobile) {
+        cardVar1.each(function () {
             var highestContent = 0;
     
-            $(cardVar2, this).find('.card-body').each(function () {
+            $(cardVar1, this).find('.card-body').each(function () {
                 if ($(this).height() > highestContent) {
                     highestContent = $(this).height();
                 }
             });
     
-            $(cardVar2, this).find('.card-body').height(highestContent + 10);
+            $(cardVar1, this).find('.card-body').height(highestContent + 10);
         });
-    }
+    // }
 })
 
 $(function () {
     var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ? true : false;
     var cardVar2 = $('.card-variant-2');
 
-    if (!isMobile) {
+    // if (!isMobile) {
         cardVar2.each(function () {
             var highestContent = 0;
     
@@ -33,5 +33,5 @@ $(function () {
     
             $(cardVar2, this).find('.card-text').height(highestContent + 16);
         });
-    }
+    // }
 })
--- src/js/components/carousel/carousel.products.js
+++ src/js/components/carousel/carousel.products.js
@@ -1,13 +1,21 @@
-import Swiper from 'swiper';
+import { Swiper, Pagination } from 'swiper';
+Swiper.use([Pagination]);
+
+var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ? true : false;
 
 $('.carousel.variant-products').each(function () {
     let $element = $(this);
+
+    var numSlides = $element.find('.swiper-container .swiper-slide').length;
+    if (numSlides <= 3 && !isMobile) {
+        $element.find('.swiper-pagination').addClass('d-none')
+    }
 
     new Swiper(this, {
         slidesPerView: 1,
         spaceBetween: 30,
         pagination: {
-            el: $element.find('.swiper-pagination'),
+            el: $element.find('.swiper-pagination').get(0),
             type: 'bullets',
         },
         breakpoints: {
