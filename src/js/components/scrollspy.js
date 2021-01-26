70eefb35c426ac8bb58c89600e1dc8729b3ed82e
ihlim97 <iehowe97@gmail.com>
2021-01-26 17:24:33 +0800

    fix scroll spyy!!

--- src/js/components/scrollspy.js
+++ src/js/components/scrollspy.js
@@ -1,15 +1,21 @@
 import { ScrollSpy } from 'bootstrap';
 
-$(function () {
+$(function () {   
+    
+    let instance;
     createSpy(document.body, '#mainNavigation');
 
     function createSpy (element, targetSelector) {        
-        window.scrollTo({ top: 0, behavior: 'auto' })
-        setTimeout(function () {
-            const instance = new ScrollSpy(element, {
-                target: targetSelector,
-                offset: $(targetSelector).height() + 10
-            })
-        }, 1000)
+        instance = new ScrollSpy(element, {
+            target: targetSelector,
+            offset: $(targetSelector).height() + 10
+        })
+    }
+
+    window.onload = function () {
+        setTimeout(() => {
+            instance.refresh()
+            instance['_offsets'] = instance['_offsets'].map(o => o += window.scrollY)
+        }, 1000);
     }
 })
